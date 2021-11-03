a96c55cc607321ff4bc336872d8bddd3bb408d5f
bovle <frederik.bl@live.dk>
2021-11-03 18:18:20 +0100

    move template to outer folder

--- bots/__init__.py
+++ bots/__init__.py
@@ -2,7 +2,7 @@ from .BotInterface import BotInterface
 
 from .sample_bots.CounterBot import CounterBot
 from .sample_bots.PercentBot import PercentBot
-from .sample_bots.TemplateBot import TemplateBot
+from .TemplateBot import TemplateBot
 
 from .testing_bots.CallBot import CallBot
 from .testing_bots.EmptyBot import EmptyBot
